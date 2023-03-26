2562b003f18d4cf4a3ca8550003cc0aed2faebd5
marwan <marwan.elansarey@gmail.com>
2023-03-26 02:52:51 +0200

    updates locations repo and create failure

--- lib/Features/Home/data/models/repo/home_repo.dart
+++
@@ -1,4 +0,0 @@
-abstract class HomeRepo {
-  fetchBestSellerBooks();
-  fetchFeatureBooks();
-}
---
+++ lib/Features/Home/data/repo/home_repo.dart
@@ -0,0 +1,8 @@
+import 'package:bookly_app/Features/Home/data/models/books_model/books_model.dart';
+import 'package:bookly_app/core/errors/failure.dart';
+import 'package:dartz/dartz.dart';
+
+abstract class HomeRepo {
+  Future<Either<Failure, List<BooksModel>>> fetchBestSellerBooks();
+  Future<Either<Failure, List<BooksModel>>> fetchFeatureBooks();
+}
