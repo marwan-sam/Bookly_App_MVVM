657730925080ee05e9c6c9c41d49e3d1845f10c1
marwan <marwan.elansarey@gmail.com>
2023-03-27 04:40:01 +0200

    srever failure for fetching from api

--- lib/Features/Home/data/repo/home_repo_impl.dart
+++ lib/Features/Home/data/repo/home_repo_impl.dart
@@ -1,21 +1,53 @@
+import 'package:bookly_app/core/utils/api_service.dart';
 import 'package:dartz/dartz.dart';
 
 import 'package:bookly_app/core/errors/failure.dart';
 
 import 'package:bookly_app/Features/Home/data/models/books_model/books_model.dart';
+import 'package:dio/dio.dart';
 
 import 'home_repo.dart';
 
 class HomeRepoTmpl implements HomeRepo {
+  final APISrevice api;
+  HomeRepoTmpl(this.api);
+
   @override
-  Future<Either<Failure, List<BooksModel>>> fetchBestSellerBooks() {
-    // TODO: implement fetchBestSellerBooks
-    throw UnimplementedError();
+  Future<Either<Failure, List<BooksModel>>> fetchNewsBooks() async {
+    try {
+      var data = await api.get(
+          endPoint:
+              'volumes?q=subject:programming&Filtering=free-ebooks&Sorting=newest');
+      List<BooksModel> books = [];
+      for (var item in data['items']) {
+        books.add(BooksModel.fromJson(item));
+      }
+      return right(books);
+    } catch (e) {
+      if (e is DioError) {
+        return left(ServerFailure.fromDioErro(e));
+      }
+
+      return left(ServerFailure(e.toString()));
+    }
   }
 
   @override
-  Future<Either<Failure, List<BooksModel>>> fetchFeatureBooks() {
-    // TODO: implement fetchFeatureBooks
-    throw UnimplementedError();
+  Future<Either<Failure, List<BooksModel>>> fetchFeatureBooks() async {
+    try {
+      var data = await api.get(
+          endPoint: 'volumes?q=subject:programming&Filtering=free-ebooks');
+      List<BooksModel> books = [];
+      for (var item in data['items']) {
+        books.add(BooksModel.fromJson(item));
+      }
+      return right(books);
+    } catch (e) {
+      if (e is DioError) {
+        return left(ServerFailure.fromDioErro(e));
+      }
+
+      return left(ServerFailure(e.toString()));
+    }
   }
 }
