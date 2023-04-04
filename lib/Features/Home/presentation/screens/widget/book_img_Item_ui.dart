f0f24ab8138016745164c90c9a53107e008b6123
marwan <marwan.elansarey@gmail.com>
2023-04-04 05:54:28 +0200

    pub add cached_network_image

--- lib/Features/Home/presentation/screens/widget/book_img_Item_ui.dart
+++ lib/Features/Home/presentation/screens/widget/book_img_Item_ui.dart
@@ -1,4 +1,5 @@
-import 'package:bookly_app/core/utils/assets.dart';
+import 'package:bookly_app/core/widgets/loading_indicator.dart';
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 
 class ImageBookItem extends StatelessWidget {
@@ -7,16 +8,16 @@ class ImageBookItem extends StatelessWidget {
   final String urlImg;
   @override
   Widget build(BuildContext context) {
-    return AspectRatio(
-      aspectRatio: 2.8 / 4,
-      child: Container(
-        decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(16),
-            // color: Colors.amberAccent,
-            image: DecorationImage(
-              fit: BoxFit.fill,
-              image: NetworkImage(urlImg),
-            )),
+    return ClipRRect(
+      borderRadius: BorderRadius.circular(16),
+      child: AspectRatio(
+        aspectRatio: 2.8 / 4,
+        child: CachedNetworkImage(
+          fit: BoxFit.fill,
+          imageUrl: urlImg,
+          placeholder: (context, url) => const LoadingIndicator(),
+          errorWidget: (context, url, error) => const Icon(Icons.error),
+        ),
       ),
     );
   }
