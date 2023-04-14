929e18d68d1284f34a3ea00a0dbcd64914400b32
marwan <marwan.elansarey@gmail.com>
2023-04-14 06:06:47 +0200

    route for screen and use extra state as model

--- lib/Features/Home/presentation/screens/widget/list_view_horizontal.dart
+++ lib/Features/Home/presentation/screens/widget/list_view_horizontal.dart
@@ -1,9 +1,11 @@
 import 'package:bookly_app/Features/Home/presentation/manager/Featured%20Books/cubit/featured_books_cubit.dart';
 import 'package:bookly_app/constants.dart';
+import 'package:bookly_app/core/utils/router_app.dart';
 import 'package:bookly_app/core/widgets/error_ui.dart';
 import 'package:bookly_app/core/widgets/loading_indicator.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:go_router/go_router.dart';
 
 import 'book_img_Item_ui.dart';
 
@@ -25,12 +27,20 @@ class FeatureListViewBooks extends StatelessWidget {
               itemBuilder: (context, index) {
                 return Padding(
                   padding: const EdgeInsets.symmetric(horizontal: 8.0),
-                  child: ImageBookItem(
-                    urlImg:
-                        state.books[index].volumeInfo.imageLinks?.thumbnail ??
-                            state.books[index].volumeInfo.imageLinks
-                                ?.smallThumbnail ??
-                            imgNotAvalible,
+                  child: GestureDetector(
+                    onTap: () {
+                      GoRouter.of(context).push(
+                        RouterApp.keyBookDetailsRoute,
+                        extra: state.books[index],
+                      );
+                    },
+                    child: ImageBookItem(
+                      urlImg:
+                          state.books[index].volumeInfo.imageLinks?.thumbnail ??
+                              state.books[index].volumeInfo.imageLinks
+                                  ?.smallThumbnail ??
+                              imgNotAvalible,
+                    ),
                   ),
                 );
               },
